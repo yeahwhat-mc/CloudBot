08d521de55f7bbbb0babd6bf370ebf86beb9dc19
Gabriel Huber <huberg18@gmail.com>
2015-06-25 06:36:02 +0200

    Fix greeting

--- plugins/greeting.py
+++ plugins/greeting.py
@@ -2,12 +2,18 @@ from cloudbot import hook
 import random
 import re
 
-GREETING_RE = re.compile(r"(\w+) joined the game\.")
 GREETINGS = ["Hallo", "Hello", "Hai", "Whoop", "Herro", "Yoyoyo", "WB", "Welcome back", "Hi", "Hellow", "Hey"]
+JOINED_RE = re.compile(r"(\w+) joined the game\.", re.IGNORECASE)
+GREETING_RE = re.compile(r"(?:{}) R2D2".format("|".join(GREETINGS)), re.IGNORECASE)
+print(r"^.*(?:{}) R2D2.*$".format("|".join(GREETINGS)))
 
-@hook.regex(GREETING_RE)
-def greet(inp):
+@hook.regex(JOINED_RE)
+def greet_joined(match):
     """Responds on greetings with a 3:1 chance"""
     if random.randint(0, 3) == 0:
-        player = inp.group(1)
-        return "{greeting} {player}!".format(greeting=random.choice(greetings), player=player)
+        player = match.group(1)
+        return "{greeting} {player}!".format(greeting=random.choice(GREETINGS), player=player)
+
+@hook.regex(GREETING_RE)
+def greet_response(match, nick):
+    return "{greeting} {player}!".format(greeting=random.choice(GREETINGS), player=nick)
